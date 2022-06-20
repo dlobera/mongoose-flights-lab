43ab240c09ba81d3b844be1e016cc852f321521b
dlobera <delobera@gmail.com>
2022-06-20 14:54:01 -0500

    create controller

---
+++ controllers/flights.js
@@ -0,0 +1,16 @@
+import {Flight} from '../models/flight.js'
+
+function newFlight (req, res) {
+  res.render("flights/new", {
+    title: "Add Flight",
+  })
+}
+
+function create(req, res) {
+
+}
+
+export {
+  newFlight as new,
+  create,
+}
