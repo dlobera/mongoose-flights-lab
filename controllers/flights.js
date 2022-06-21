e8da067e5bd6de446aeeaf44a08247ecd9c84980
dlobera <delobera@gmail.com>
2022-06-21 09:14:25 -0500

    create function

--- controllers/flights.js
+++ controllers/flights.js
@@ -7,8 +7,19 @@ function newFlight (req, res) {
 }
 
 function create(req, res) {
-
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+	}
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect(`/flights`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
 }
+
 
 export {
   newFlight as new,
